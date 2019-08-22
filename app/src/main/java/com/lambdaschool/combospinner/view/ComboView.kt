7cba0f4364bed97296625cfea7d1801d854b13b3
Chance Payne <chance@lambdaschool.com>
2019-08-22 10:23:21 -0700

    define and use attributes for the text size and weight

--- app/src/main/java/com/lambdaschool/combospinner/view/ComboView.kt
+++ app/src/main/java/com/lambdaschool/combospinner/view/ComboView.kt
@@ -10,15 +10,15 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.lambdaschool.combospinner.R
 
-class ComboView: LinearLayout {
+class ComboView(context: Context, attrs: AttributeSet?): LinearLayout(context, attrs) {
 
-    constructor(context: Context): super(context)
+    constructor(context: Context): this(context, null)
 
-    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+//    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+//    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
+//    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
 
     val listItems = listOf("Cat", "o'nine", "tails", "topmast", "loot", "Nelsons", "folly", "code", "of", "conduct", "bilge", "scuppers", "barque","spanker", "warp")
     var pointer = 0
@@ -38,11 +38,17 @@ class ComboView: LinearLayout {
     init {
         /*val text = TextView(context)
         addView(text)*/
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ComboView)
+        val textWeight = typedArray.getFloat(R.styleable.ComboView_layout_weight, 8f)
+        val textSize = typedArray.getDimension(R.styleable.ComboView_textSize, 12f)
+        typedArray.recycle()
 
-        val textLayoutParams = LayoutParams(0, LayoutParams.WRAP_CONTENT, 8f)
+        val textLayoutParams = LayoutParams(0, LayoutParams.WRAP_CONTENT, textWeight)
         val textView = LayoutInflater.from(context).inflate(R.layout.comboview_layout, this, false) as TextView
         textView.text = listItems[pointer]
         textView.layoutParams = textLayoutParams
+        textView.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+        textView.textSize = textSize
 
 
         val imageLayoutParams = LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1f)
