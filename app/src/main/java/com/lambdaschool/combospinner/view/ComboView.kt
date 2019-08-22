debec391376079563bb92cee9686b7d6560952b4
Chance Payne <chance@lambdaschool.com>
2019-08-22 10:05:18 -0700

    add layout parameters to improve layout look

--- app/src/main/java/com/lambdaschool/combospinner/view/ComboView.kt
+++ app/src/main/java/com/lambdaschool/combospinner/view/ComboView.kt
@@ -3,6 +3,7 @@ package com.lambdaschool.combospinner.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -19,23 +20,51 @@ class ComboView: LinearLayout {
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
 
+    val listItems = listOf("Cat", "o'nine", "tails", "topmast", "loot", "Nelsons", "folly", "code", "of", "conduct", "bilge", "scuppers", "barque","spanker", "warp")
+    var pointer = 0
+    fun incrementPointer() {
+        pointer++
+        if(pointer >= listItems.size) {
+            pointer = 0
+        }
+    }
+    fun decrementPointer() {
+        pointer--
+        if(pointer < 0) {
+            pointer = listItems.size - 1
+        }
+    }
 
     init {
         /*val text = TextView(context)
         addView(text)*/
 
+        val textLayoutParams = LayoutParams(0, LayoutParams.WRAP_CONTENT, 8f)
         val textView = LayoutInflater.from(context).inflate(R.layout.comboview_layout, this, false) as TextView
-        textView.text = "Sample Text"
+        textView.text = listItems[pointer]
+        textView.layoutParams = textLayoutParams
 
+
+        val imageLayoutParams = LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1f)
         val previousImageView = ImageView(context)
+        previousImageView.layoutParams = imageLayoutParams
         previousImageView.setImageDrawable(ContextCompat.getDrawable(context, android.R.drawable.ic_media_previous))
+        previousImageView.setOnClickListener {
+            decrementPointer()
+            textView.text = listItems[pointer]
+        }
 
         val nextImageView = ImageView(context)
         nextImageView.setImageDrawable(ContextCompat.getDrawable(context, android.R.drawable.ic_media_next))
+        nextImageView.layoutParams = imageLayoutParams
+        nextImageView.setOnClickListener {
+            incrementPointer()
+            textView.text = listItems[pointer]
+        }
 
         orientation = HORIZONTAL
         addView(previousImageView)
-        addView(nextImageView)
         addView(textView)
+        addView(nextImageView)
     }
 }
